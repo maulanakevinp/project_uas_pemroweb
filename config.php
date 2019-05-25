8d05747dd9cecdcac40de17bf51435b4f5e392ed
Maulana Kevin Pradana <44237629+maulanakevinpradana@users.noreply.github.com>
2019-05-25 12:35:34 +0700

    update db online

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$mysqli = new mysqli("localhost","root","");
-$mysqli->select_db("malasngoding_kios");
+$mysqli = new mysqli("www.db4free.net","manenin","manenin1234");
+$mysqli->select_db("db_manenin");
 ?>
