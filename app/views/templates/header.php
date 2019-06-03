ebee61504731d4c3e5aadea35dd23cf771bc01fc
Maulana Kevin Pradana <44237629+maulanakevinpradana@users.noreply.github.com>
2019-06-03 12:04:42 +0700

    update

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -25,26 +25,32 @@
 </head>
 
 <body>
-	<header class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-		<a class="navbar-brand" href="#">Manenin</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+	<header class="navbar navbar-expand-md navbar-dark fixed-top bg-white border-bottom shadow-sm">
+		<h5 class="my-0 mr-md-auto font-weight-normal">Manenin</h5>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars" aria-controls="navbars" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
+		<div class="collapse navbar-collapse" id="navbars">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kategori</a>
-					<div class="dropdown-menu" aria-labelledby="dropdown01">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<a class="dropdown-item" href="#">Something else here</a>
+					<a class="nav-link dropdown-toggle ml-3 mt-1" href="#" id="kategori" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black">Kategori</a>
+					<div class="dropdown-menu" aria-labelledby="kategori">
+						<a class="dropdown-item" href="#">Sektor Pertanian</a>
+						<a class="dropdown-item" href="#">Sektor Perkebunan</a>
+						<a class="dropdown-item" href="#">Sektor Tanaman</a>
 					</div>
 				</li>
 			</ul>
-			<form class="form-inline my-2 my-lg-0">
-				<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-			</form>
+			<div class="input-group mr-3 ml-3">
+				<input type="text" class="form-control" placeholder="Cari Buah-buahan atau Sayuran" aria-label="Cari Buah-buahan atau Sayuran" aria-describedby="button-addon2">
+				<div class="input-group-append">
+					<button class="btn btn-outline-secondary" type="submit" id="button-addon2">Cari</button>
+				</div>
+			</div>
+
+			<div class="btn-group ">
+				<a class="btn btn-outline-success" href="#">Masuk</a>
+				<a class="btn btn-outline-primary" href="#">Daftar</a>
+			</div>
 		</div>
 	</header>
